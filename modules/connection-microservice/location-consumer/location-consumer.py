2e492bc0fa7a4ab2166ae85421081909af687d03
Fady Refaat <fadyrefaatzaky@gmail.com>
2022-08-13 11:00:01 +0200

    fix location consumer

--- modules/connection-microservice/location-consumer/location-consumer.py
+++ modules/connection-microservice/location-consumer/location-consumer.py
@@ -1,19 +1,33 @@
 import os
+import json
 from kafka import KafkaConsumer
-from typing import List
-from geoalchemy2.functions import ST_Point
-from app.udaconnect_connection.models import  Location
-from app import db
+from sqlalchemy import create_engine 
 
+DB_USER = os.environ["DB_USERNAME"]
+DB_PASSWORD  = os.environ["DB_PASSWORD"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_NAME = os.environ["DB_NAME"]
+
+KAFKA_URL = os.environ["KAFKA_URL"]
 KAFKA_TOPIC = os.environ["KAFKA_TOPIC"]
 
-consumer = KafkaConsumer(KAFKA_TOPIC)
-for message in consumer:
-    location = message.value.decode("UTF-8")
-    new_location = Location()
-    new_location.person_id = location["person_id"]
-    new_location.creation_time = location["creation_time"]
-    new_location.coordinate = ST_Point(location["latitude"], location["longitude"])
-    db.session.add(new_location)
-    db.session.commit()
-    locations: List = db.session.query(Location).all()
+consumer = KafkaConsumer(KAFKA_TOPIC,bootstrap_servers=[KAFKA_URL])
+
+db = create_engine(
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+dbConnection = db.connect()
+
+
+
+for location in consumer:
+    msg = location.value.decode('utf-8')
+    location_msg = json.loads(msg)
+    
+    person_id = int(location_msg["person_id"])
+    latitude = int(location_msg["latitude"])
+    longitude = int(location_msg["longitude"])
+
+    insertLocation = "INSERT INTO location (person_id, coordinate) VALUES ({},ST_Point({}, {}))".format(person_id, latitude, longitude)
+    dbConnection.execute(insertLocation)
